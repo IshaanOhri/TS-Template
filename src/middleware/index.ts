6b6a3650098baacce47385caa91dd5261ed00693
Ishaan Ohri <ishaan99ohri@gmail.com>
2021-02-03 14:31:25 +0530

    added all basic files

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -2,6 +2,8 @@
  * @Author: Ishaan Ohri
  * @Date: 2021-02-03 14:14:48
  * @Last Modified by: Ishaan Ohri
- * @Last Modified time: 2021-02-03 14:16:31
+ * @Last Modified time: 2021-02-03 14:27:14
  * @Description: Indexing file for all Middleware
  */
+
+export * from './handlers';
